97d06b030e9a5253cd8b83c0d3e92c07613dcc55
DESKTOP-CF4O65H\Away <875229565@qq.com>
2016-10-26 18:36:00 +0800

    增加哈希函数

---
+++ scc/scc/elfhash.c
@@ -0,0 +1,19 @@
+#include"scc.h"
+/******************************/
+
+
+int elf_hash(char* key)
+{
+	int h = 0, g;
+	while (*key)
+	{
+		h = (h << 4) + *key++;
+		g = h & 0xf0000000;
+		if (g)
+		{
+			h ^= g >> 24;
+		}
+		h &= ~g;
+	}
+	return h % MAXKEY;
+}
