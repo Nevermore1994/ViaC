7f5d91b8a09891b0a6b5a351b9a52d0ad399a019
DESKTOP-CF4O65H\Away <875229565@qq.com>
2016-10-28 20:31:42 +0800

    添加测试代码

---
+++ scc/scc/test.c
@@ -0,0 +1,19 @@
+#include"scc.h"
+
+int main(int argc, char ** argv)
+{
+
+	errno_t err = fopen_s(&fin, "C:\\Users\\Away\\Documents\\scc\\scc\\Debug\\1.c", "rb");
+	if (err)
+	{
+		printf("不能打开SC源文件!\n");
+		return 0;
+	}
+	init();
+	getch();
+	test_lex();
+	cleanup();
+	fclose(fin);
+	printf("%s 词法分析成功!", argv [1]);
+	return 1;
+}
