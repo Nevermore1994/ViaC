0223b03ec23b8b7814e4440421e2647f73f4066d
DESKTOP-CF4O65H\Away <875229565@qq.com>
2016-11-08 13:36:46 +0800

    添加机器语言代码

--- scc/scc/scc.h
+++ scc/scc/scc.h
@@ -186,88 +186,6 @@ int ElfHash(char* key);				// 字符哈希函数
 int CalcAlign(int n, int align);
 
 /********************end*******************/
-/*****************语法分析*********************/
-enum e_SynTaxState
-{
-	SNTX_NUL,
-	SNTX_SP, 
-	SNTX_LF_HT, 
-	SNTX_DELAY
-};
-
-enum  e_StorageClass
-{
-	SC_GLOBAL  = 0x00f0, 
-	SC_LOCAL   = 0x00f1, 
-	SC_LLOACL  = 0x00f2, 
-	SC_CMP     = 0x00f3,
-	SC_VALMASK = 0x00ff,
-	SC_LVAL    = 0x0100,//左值
-	SC_SYM     = 0x0200,//符号
-	 
-	SC_ANOM    = 0x10000000,  //匿名符号
-	SC_STRUCT  = 0x20000000,  //结构体
-	SC_MEMBER  = 0x40000000,  //结构体的成员变量
-	SC_PARAMS  = 0x80000000,  //函数参数
-};
-
-enum  e_TypeCode
-{
-	T_INT    = 0,
-	T_CHAR   = 1, 
-	T_SHORT  = 2,
-	T_VOID   = 3, 
-	T_PTR    = 4, 
-	T_FUNC   = 5, 
-	T_STRUCT = 6, 
-
-	T_BTYPE  = 0x000f, 
-	T_ARRAY  = 0x0010,
-};
-
-#define ALIGN_SET 0x100
-
-extern int syntax_state;
-extern int syntax_level;
-
-void TranslationUnit(void);
-void ExternalDeclaration(const int level);
-void Initializer(Type* type); // 初值符
-int TypeSpecifier(Type* type);
-void StructSpecifier(Type* type);
-void StructDeclarationList(Type* type);
-void StructDeclaration(int* maxalign, int* offset, Symbol*** ps);
-void Declarator(Type* type, const int* v, const int* force_align);
-void FunctionCallingConvention(int* fc);
-void StructMemberAlignment(int* force_align);
-void DirectDeclarator(Type* type, int* v, const int func_call);
-void DirectDeclaratorPostfix(Type* type, const int func_call);
-void ParameterTypeList(Type* type, int func_call);
-void Funcbody(Symbol* sym);
-int IsTypeSpecifier(const int id);
-void Statement(int* bsym, int* csym);
-void CompoundStatement(int* bsym, int* csym);
-void IfStatement(const int* bsym, const int* csym);
-void ForStatement(const int* bsym, const int* csym);
-void ContinueStatement(const int* csym);
-void BreakStatement(const int* bsym);
-void ReturnStatement(void);
-void ExpressionStatement(void);
-void Expression(void);
-void AssignmentExpression(void);
-void EqualityExpression(void);
-void RelationalExpression(void);
-void AdditiveExpression(void);
-void MultiplicativeExpression(void);
-void UnaryExpression(void);
-void SizeofExpression(void);
-void PostfixExpression(void);
-void PrimaryExpression(void);
-void ArgumentExpressionList(void);
-void PrintTab(const int num);
-void SyntaxIndent(void);
-/*******************end*************************/
-
 
 /******************Stack*********************/
 typedef struct Stack
@@ -319,9 +237,90 @@ Symbol* FuncSymPush(int v, Type *type);
 Symbol* VarSymPut(Type* type, int r, int v, int addr);
 Symbol* SecSymPut(char* sec, int c);
 void SymPop(Stack* stack, Symbol *b);
-
 int TypeSize(Type *t, int *a);
 /********************end**********************/
+/*****************语法分析*********************/
+enum e_SynTaxState
+{
+	SNTX_NUL,
+	SNTX_SP,
+	SNTX_LF_HT,
+	SNTX_DELAY
+};
+
+enum  e_StorageClass
+{
+	SC_GLOBAL = 0x00f0,
+	SC_LOCAL = 0x00f1,
+	SC_LLOACL = 0x00f2,
+	SC_CMP = 0x00f3,
+	SC_VALMASK = 0x00ff,
+	SC_LVAL = 0x0100,//左值
+	SC_SYM = 0x0200,//符号
+
+	SC_ANOM = 0x10000000,  //匿名符号
+	SC_STRUCT = 0x20000000,  //结构体
+	SC_MEMBER = 0x40000000,  //结构体的成员变量
+	SC_PARAMS = 0x80000000,  //函数参数
+};
+
+enum  e_TypeCode
+{
+	T_INT = 0,
+	T_CHAR = 1,
+	T_SHORT = 2,
+	T_VOID = 3,
+	T_PTR = 4,
+	T_FUNC = 5,
+	T_STRUCT = 6,
+
+	T_BTYPE = 0x000f,
+	T_ARRAY = 0x0010,
+};
+
+#define ALIGN_SET 0x100
+
+extern int syntax_state;
+extern int syntax_level;
+
+void TranslationUnit(void);
+void ExternalDeclaration(const int level);
+void Initializer(Type* type); // 初值符
+int TypeSpecifier(Type* type);
+void StructSpecifier(Type* type);
+void StructDeclarationList(Type* type);
+void StructDeclaration(int* maxalign, int* offset, Symbol*** ps);
+void Declarator(Type* type, const int* v, const int* force_align);
+void FunctionCallingConvention(int* fc);
+void StructMemberAlignment(int* force_align);
+void DirectDeclarator(Type* type, int* v, const int func_call);
+void DirectDeclaratorPostfix(Type* type, const int func_call);
+void ParameterTypeList(Type* type, int func_call);
+void Funcbody(Symbol* sym);
+int IsTypeSpecifier(const int id);
+void Statement(int* bsym, int* csym);
+void CompoundStatement(int* bsym, int* csym);
+void IfStatement(const int* bsym, const int* csym);
+void ForStatement(const int* bsym, const int* csym);
+void ContinueStatement(const int* csym);
+void BreakStatement(const int* bsym);
+void ReturnStatement(void);
+void ExpressionStatement(void);
+void Expression(void);
+void AssignmentExpression(void);
+void EqualityExpression(void);
+void RelationalExpression(void);
+void AdditiveExpression(void);
+void MultiplicativeExpression(void);
+void UnaryExpression(void);
+void SizeofExpression(void);
+void PostfixExpression(void);
+void PrimaryExpression(void);
+void ArgumentExpressionList(void);
+void PrintTab(const int num);
+void SyntaxIndent(void);
+/*******************end*************************/
+
 /********************coff***********************/
 
 #pragma pack(push,1)
@@ -382,7 +381,16 @@ void InitCoff(void);
 void Fpad(const FILE* fp, const int new_pos);
 void WriteObj(const char* name);
 /*********************end**************************/
-#endif // !SCC_H_
+typedef struct Operand
+{
+	Type type; 
+	unsigned short reg;
+	int value;
+	struct Symbol* sym;
+}Operand;
+
+
+#endif // SCC_H_
 
 
 
