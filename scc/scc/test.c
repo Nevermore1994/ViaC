33cfcca5b3c8e8a0509b290437470c3059a69e53
DESKTOP-CF4O65H\Away <875229565@qq.com>
2016-11-13 23:10:40 +0800

    添加测试代码

--- scc/scc/test.c
+++ scc/scc/test.c
@@ -9,7 +9,7 @@ int main(const int argc, char ** argv)
 {
 	
 	Init();
-	filename = "C:\\Users\\Away\\Documents\\scc\\scc\\Debug\\1.viac";
+	filename = "C:\\Users\\Away\\Documents\\scc\\scc\\Debug\\HelloWorld.c";
 	errno_t err = fopen_s(&fin, filename, "rb");
 	if (err)
 	{
@@ -22,9 +22,11 @@ int main(const int argc, char ** argv)
 	TranslationUnit();
 	fclose(fin);
 
-	//GetObjFname(filename);
-	//WriteObj(outfile);
-	printf("%s\n语法分析成功!\n",filename);
+	GetObjFname(filename);
+	WriteObj(outfile);
+	char file[2048];
+	sprintf_s(file, 2048, "C:\\Users\\Away\\Documents\\scc\\scc\\Debug\\1.viac%c",'\0' );
+	printf("%s\n语法分析成功!\n",file);
 	Cleanup();
 	return 1;
 }
